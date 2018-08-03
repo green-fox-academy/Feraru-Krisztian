50bcd2f66bb2a8dd44ed86705b6902dcdc7366c5
Feraru Krisztián <36930214+kovkrasz@users.noreply.github.com>
2018-08-03 14:01:20 +0200

    commit

---
+++ week-01/day-4/Exercise24/src/PrintEven.java
@@ -0,0 +1,11 @@
+public class PrintEven {
+    public static void main(String[] args) {
+
+        int e = 0;
+        while (e <= 500) {
+            System.out.print(" " + e);
+            e += 2;
+        }
+
+    }
+}
