79dfdf1b5631e8af72de7fe701b1f68c8a1d6ded
Feraru Krisztián <36930214+kovkrasz@users.noreply.github.com>
2018-08-06 16:03:17 +0200

    week02/day01/appedaAdded commit

---
+++ week-02/Day-01/Exercise03/src/AppendA.java
@@ -0,0 +1,13 @@
+public class AppendA {
+    public static void main(String[] args) {
+
+        String typo = "ChinChill";
+        System.out.println(appendAFunc(typo));
+
+    }
+
+    public static String appendAFunc(String input) {
+
+        return input + "a";
+    }
+}
