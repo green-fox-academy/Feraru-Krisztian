e929cc5ce426966a52462fae6c6b85678f26d28a
Feraru Krisztián <36930214+kovkrasz@users.noreply.github.com>
2018-08-06 17:29:08 +0200

    ThirdExample commit

---
+++ week-02/Day-01/Exercise07/src/Third.java
@@ -0,0 +1,11 @@
+public class Third {
+    public static void main(String[] args) {
+
+        int[] q = {4, 5, 6, 7};
+        a(q);
+    }
+    public static void a(int[] intArray) {
+
+        System.out.println(intArray[2]);
+    }
+}
