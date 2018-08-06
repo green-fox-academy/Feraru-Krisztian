7ead69cebdd5d7f84470473f40dee3d599ce6ddf
Feraru Krisztián <36930214+kovkrasz@users.noreply.github.com>
2018-08-06 20:43:09 +0200

    incrementElementExample commit

--- week-02/Day-01/Exercise11/src/IncrementElement.java
+++ week-02/Day-01/Exercise11/src/IncrementElement.java
@@ -1,7 +1,8 @@
-public class ChangeElement {
+public class IncrementElement {
     public static void main(String[] args) {
 
         int[] t = {1, 2, 3, 4, 5, 6};
+        t[2] ++;
 
         System.out.println(t[2]);
 
