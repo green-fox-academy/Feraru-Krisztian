b745e5d5e682210937f04be8b2311091e87e6f6c
Feraru Krisztián <36930214+kovkrasz@users.noreply.github.com>
2018-08-06 15:16:29 +0200

    week02/day01/firstcommit

---
+++ week-02/Day-01/Exercise01/src/Doubling.java
@@ -0,0 +1,17 @@
+public class Doubling {
+    public static void main(String[] args) {
+        // - Create an integer variable named `baseNum` and assign the value `123` to it
+        // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+        // - Print the result of `doubling(baseNum)`
+
+        int baseNum = 123;
+        int result = doubling(baseNum);
+        System.out.println(result);
+    }
+    public static int doubling(int toBeDoubled) {
+
+        return toBeDoubled * 2;
+
+    }
+
+}
