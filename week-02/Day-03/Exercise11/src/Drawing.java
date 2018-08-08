a294098c6b2ad00d92191d23032b073e0cfb3225
Feraru Krisztián <36930214+kovkrasz@users.noreply.github.com>
2018-08-08 17:42:36 +0200

    purpleSteps commit

---
+++ week-02/Day-03/Exercise11/src/Drawing.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Drawing {
+    public static void mainDraw(Graphics graphics) {
+        // Reproduce this:
+        // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+
+        int x =0;
+        int y = 0;
+        int size = 10;
+        graphics.setColor(new Color(140, 0, 211));
+        for(int i = 1; i < 20; i++) {
+            graphics.fillRect(i * size, i*size, size, size);
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
