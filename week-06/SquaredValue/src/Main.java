64b048dcba6f497b67999a355521fda0f9d16a2a
Feraru Krisztián <36930214+kovkrasz@users.noreply.github.com>
2018-09-10 14:54:26 +0200

    Squared value exercise done

---
+++ week-06/SquaredValue/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> myList = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+         myList.stream()
+                .filter(x -> x > 0)
+                 .mapToInt(x -> x * x)
+                 .forEach(System.out::println);
+    }
+}
