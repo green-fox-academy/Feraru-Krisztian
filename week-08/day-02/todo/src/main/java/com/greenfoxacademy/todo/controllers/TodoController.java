e34b711923c1fd414ee0b8a9dca371cab40c135d
Feraru Krisztián <36930214+kovkrasz@users.noreply.github.com>
2018-09-25 17:17:21 +0200

    List the unfinished todos

--- week-08/day-02/todo/src/main/java/com/greenfoxacademy/todo/controllers/TodoController.java
+++ week-08/day-02/todo/src/main/java/com/greenfoxacademy/todo/controllers/TodoController.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class TodoController {
@@ -18,12 +20,12 @@ public class TodoController {
     public TodoController(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
     }
-    @RequestMapping("/todo")
-    public String todos(Model model) {
-        model.addAttribute("todoList", todoRepository.findAll());
-
-        return "index";
-    }
+//    @RequestMapping("/todo")
+//    public String todos(Model model) {
+//        model.addAttribute("todoList", todoRepository.findAll());
+//
+//        return "index";
+//    }
 
     @RequestMapping("/todo/add")
     public String getAddTodo() {
@@ -39,5 +41,20 @@ public class TodoController {
         return "redirect:/todo";
     }
 
+    @RequestMapping("/todo")
+    public String isAktive( Model model, @RequestParam(value = "isActive", required = false) String isActive){
+        List<Todo> doneTodos = new ArrayList<>();
+        for (Todo todo : todoRepository.findAll()){
+            if(!todo.isDone()) {
+                doneTodos.add(todo);
+            }
+        }
+        if (isActive == null) {
+            model.addAttribute("todoList", todoRepository.findAll());
+        } else if (isActive.equals("true")) {
+            model.addAttribute("todoList", doneTodos);
+        }
+        return "index";
+    }
 
 }
