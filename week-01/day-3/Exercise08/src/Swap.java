cb6779cdf3f203acbb3d111ffbca7f25b1abebcf
Feraru Krisztián <36930214+kovkrasz@users.noreply.github.com>
2019-01-09 16:59:01 +0100

    fix(): Swap

--- week-01/day-3/Exercise08/src/Swap.java
+++ week-01/day-3/Exercise08/src/Swap.java
@@ -2,6 +2,10 @@ public class Swap {
     public static void main(String[] args) {
         int a = 526;
         int b = 123;
+        int a1 = 526;
+
+        a = b;
+        b = a1;
 
         System.out.println(a);
         System.out.println(b);
