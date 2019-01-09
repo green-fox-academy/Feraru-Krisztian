7a7383efabb3e890a52afd971710d9ba974f1db8
Feraru Krisztián <36930214+kovkrasz@users.noreply.github.com>
2019-01-09 16:49:46 +0100

    fix(): coding hours

--- week-01/day-3/Exercise06/src/CodingHours.java
+++ week-01/day-3/Exercise06/src/CodingHours.java
@@ -5,7 +5,5 @@ public class CodingHours {
         int weeksPerSemester = 17;
         int spendHoursCodingPerSemester = hoursPerDay * daysPerWeek * weeksPerSemester;
         System.out.println(spendHoursCodingPerSemester);
-        int hoursPerWeek = 52;
-        System.out.println(spendHoursCodingPerSemester % hoursPerWeek);
     }
 }
